e37ba301b7ea4101b82c2ad008ca8cad92843337
DIOGO DE ANDRADE CHELLES <40156119854@info.local>
2026-05-29 10:46:23 -0300

    :sparkles: feat: Implementado modo "Fan" e modo "Ligar"

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,17 @@
+/* 
+Autores:
+Augusto Vicente Santos
+Diogo De Andrade Chelles
+Luanda da Silva Leite
+Lucas de Oliveira Donega
+
+Data de entrega do projeto: 5 de junho de 2026
+Versão: 1.0
+
+Programa: Controle do ar Condicionado Via MQTT
+Descrição: Controlaremos um ar condicionado via mqtt com um ESP32 e um infravermelho com LED, aplicando os modos FAN, AUTO e COOL, ligando e desligando o ar Condicionado. Quando um Json for enviado para o Nosso ESP32, esse microcontrolador vai ativar o infravermelho, enviando o sinal solicitado (pelo usuário) para o ar condicionado
+*/
+
 //*main.cpp
 
 #include <Arduino.h>
@@ -10,20 +24,32 @@
 #include "MqttManager.h"
 #include "DebugManager.h"
 
+void modoFan();
 //*=======constantes==========
+
+const uint16_t pinLed = 18;
+
+// Objeto Fujitsu
+IRFujitsuAC ac(pinLed);
 
 const char TOPICO_COMANDO[] = "senai134/esp32/comando";
 
-void tratarMensagemRecebida(const char *topico, const String &mensagem);
-void tratarJsonComando(const String &mensagem);
+const int boot = 0;
+bool estadoAnteriorBoot = HIGH;
+bool estadoAtualBoot = digitalRead(boot);
+
+// void tratarMensagemRecebida(const char *topico, const String &mensagem);
+// void tratarJsonComando(const String &mensagem);
 
 void setup()
 {
   configurarDebug();
   conectarWiFi();
   configurarMQTT();
-  registrarCallbackMensagem(tratarMensagemRecebida);
+  // registrarCallbackMensagem(tratarMensagemRecebida);
   conectarMQTT();
+
+  pinMode(boot, INPUT_PULLUP);
 }
 
 void loop()
@@ -31,6 +57,51 @@ void loop()
   garantirWiFiConectado();
   garantirMQTTconectado();
   loopMQTT();
+
+  // Envia comando para o infravermelho
+  ac.send();
+  ac.begin();
+
+  if (estadoAnteriorBoot == HIGH && estadoAtualBoot == 0)
+  {
+    ac.on();
+    ac.send();
+    estadoAnteriorBoot = estadoAtualBoot;
+  }
+}
+
+void modoFan()
+{
+  // Temperatura
+  ac.setTemp(23);
+
+  switch (kFujitsuAcModeFan)
+  {
+  case 1: // FAN HIGH
+    ac.setFanSpeed(kFujitsuAcFanHigh);
+    Serial.println("Modo FAN HIGH enviado!");
+    break;
+
+  case 2: // FAN LOW
+    ac.setFanSpeed(kFujitsuAcFanLow);
+    Serial.println("Modo FAN LOW enviado!");
+    break;
+
+  case 3: // FAN QUIET
+    ac.setFanSpeed(kFujitsuAcFanQuiet);
+    Serial.println("Modo FAN QUIET enviado!");
+    break;
+
+  case 4: // FAN MEDIO
+    ac.setFanSpeed(kFujitsuAcFanMed);
+    Serial.println("Modo FAN MEDIO enviado!");
+    break;
+
+  case 5: // FAN AUTO
+    ac.setFanSpeed(kFujitsuAcFanAuto);
+    Serial.println("Modo FAN AUTO enviado!");
+    break;
+  }
 }
 
 void tratarMensagemRecebida(const char *topico, const String &mensagem)
